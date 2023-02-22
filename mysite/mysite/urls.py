5170e0908531ac72c3a35b2a59009ec7aae66d73
Quinn Britton <qbritton1@gmail.com>
2023-02-22 18:55:13 +0000

    fixed index page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='admin/')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
