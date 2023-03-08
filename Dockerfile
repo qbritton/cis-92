e0500b98d4d62a825dddab2941fd041d4a596e68
Quinn Britton <qbritton1@gmail.com>
2023-03-08 20:34:19 +0000

    Edited for Cloud Run

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ RUN pip install django
 COPY mysite /mysite 
 WORKDIR /mysite 
 CMD python ./manage.py runserver 0.0.0.0:8000
+ENV PORT=8080
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3zbarr8r7e=56bvs88o1+0wo5q$f+0uy$vcwd*2-8d97y)wwt!'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [ "https://*.run.app" ]
 
 
 # Application definition
